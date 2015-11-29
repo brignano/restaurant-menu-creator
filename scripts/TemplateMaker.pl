0434e6702f7279b2dce9c669d63b0e6f5a5c6c03
Kogurr <kogurr@gmail.com>
2015-11-28 21:15:59 -0500

    Refactored the complex storage mechanism for extract_content. Added in the HTML generation code for menu entries using the template as a basis. Tested and working with a sample menu.

--- scripts/TemplateMaker.pl
+++ scripts/TemplateMaker.pl
@@ -38,6 +38,7 @@ unless ($template && $pdf_name && $menu_title) {
 
 exit make_pdf();
 
+
 ## ======
 ## Uses Mojo::DOM to edit the template HTML file with the data supplied
 ## through the command line arguments.
@@ -51,6 +52,12 @@ sub make_pdf {
 		"$template.html"
 	);
 
+	my $css_path = catfile(
+		$FindBin::Bin,
+		$template,
+		"$template.css"
+	);
+
 	my $pdf_output = catfile(
 		$FindBin::Bin,
 		"$pdf_name.pdf"
@@ -62,7 +69,7 @@ sub make_pdf {
 	{ local $/; $html_content = <$html_fh> };
 	close($html_fh) or die('Couldn\'t close file');
 
-	# Use Mojo::DOM to edit the HTML template
+	# Load the HTML template into the DOM parser for editing
 	my $dom = Mojo::DOM->new($html_content);
 
 	# Add the menu name
@@ -72,68 +79,90 @@ sub make_pdf {
 
 	### TODO: Add logo if one was given
 
-	my $extract = extract_content();
+	my $extract       = extract_content();
+	my $submenu_index = 0;
+	my $mimic         = $dom->at('div.menu-section');
 
-	# Copy and modify the template for a subsection
-	my $subsection = '2';
-	my $mimic      = $dom->at('div#menu-section-1');
-	my $inner_dom  = Mojo::DOM->new($mimic);
+	$dom->at('div.menu-section')
+		->remove
+		->root;
 
-	$inner_dom->at('div#menu-section-1')
-		->attr(id => "menu-section-$subsection");
+	# Generate the submenu HTML from the supplied content arguments
+	for my $submenu (@$extract) {
+		my $inner_dom     = Mojo::DOM->new($mimic);
+		my $elem_number   = 1;
+		my $subsection    = $submenu_index + 1;
 
-	for my $z (1 .. 4) {
-		my $element_html = qq {
-			<li class="section-$subsection-element" id="section-$subsection-e$z">
-				<ul class="section-sublist">
-					<li class="section-title"></li>
-					<li class="section-desc"></li>
-				</ul>
-			</li>
-		};
+		$inner_dom->at('div.menu-section')
+			->attr(id => "menu-section-$subsection");
+		$inner_dom->at('h2.submenu-title')
+			->attr(id => "section-$subsection-header");
+		$inner_dom->at("h2#section-$subsection-header")
+			->content($submenu->{'head'})
+			->root;
 
-		$inner_dom->at('ul#element-list')
-			->append_content($element_html)
+		for my $elem (keys %{$submenu->{'sections'}}) {
+			my $elem_info = $submenu->{'sections'}->{$elem};
+			# Price should always be passed after the description
+			my $elem_desc = $elem_info->[0];
+			# my $elem_price = $elem_info->[1];
+
+			my $element_html = qq {
+				<li class="section-$subsection-element" id="section-$subsection-e$elem_number">
+					<ul class="section-sublist">
+						<li class="section-title">$elem</li>
+						<li class="section-desc">$elem_desc</li>
+					</ul>
+				</li>
+			};
+
+			$inner_dom->at('ul#element-list')
+				->append_content($element_html)
+				->root;
+			$elem_number++;
+		}
+
+		$submenu_index++;
+		$dom->at('body')
+			->append_content($inner_dom)
 			->root;
 	}
 
-	say $inner_dom;
-
-	### DEBUG: Don't create a pdf file
-	return 1;
-
-	# Generate the PDF from the customized template
+	# Use the wkhtmltopdf wrapper to create the PDF menu
 	PDF::WebKit->configure(sub {
 		$_->wkhtmltopdf('C:/wkhtmltopdf/bin/wkhtmltopdf.exe');
 	});
 
+	### TODO: Customize the desired PDF settings, these are just for testing.
 	my $webkit = PDF::WebKit->new(
 		\$dom->to_string,
 		page_size    => 'Letter',
 		footer_right => '[date]'
 	);
+	push @{$webkit->stylesheets}, $css_path;
 
 	$webkit->to_file($pdf_output);
 	return 1;
 }
 
 ## ======
-## Creates a hash using each subsection title as a key which has
-## a value containing another hash.
+## Creates an array which contains hashref elements.
 ##
-## The second hash uses the element title as the key and has
-## a value containing the description of the element.
+## The hashref uses the element title as the key and has
+## an arrayref value containing the description and price
+## of the element.
 ##
-## Returns a hashref
+## Returns an arrayref.
 ## ======
 sub extract_content {
-	my %extracted_content;
+	my @extracted_content;
 
 	my $header_token = '*';
 	my $item_token   = '^';
 	my $desc_token   = '!';
-
-	my ($c_header_key, $c_item_key);
+	my $price_token  = '$';
+	my $index = 0;
+	my ($c_header_index, $c_item_key);
 
 	while (scalar @content > 0) {
 		my $line  = shift @content;
@@ -141,20 +170,29 @@ sub extract_content {
 		my $token = substr $line, 0, 1;
 
 		if ($token eq $header_token) {
-			$extracted_content{$input} = {};
-			$c_header_key = $input;
+			push @extracted_content, {
+				head     => $input,
+				sections => {}
+			};
+			$c_header_index = $index;
+			$index++;
 		}
 
 		elsif ($token eq $item_token) {
-			$extracted_content{$c_header_key}->{$input} = 0;
+			my %c_hash = %{$extracted_content[$c_header_index]};
+			$c_hash{'sections'}->{$input} = [];
+			$extracted_content[$c_header_index] = \%c_hash;
 			$c_item_key = $input;
 		}
 
 		elsif ($token eq $desc_token) {
-			### TODO: Change to an array which we expect to hold two elements, desc + price
-			$extracted_content{$c_header_key}->{$c_item_key} = $input;
+			push @{$extracted_content[$c_header_index]{'sections'}->{$c_item_key}}, $input;
+		}
+
+		elsif ($token eq $price_token) {
+			say "PRICE $input";
 		}
 	}
 
-	return \%extracted_content;
+	return \@extracted_content;
 }
