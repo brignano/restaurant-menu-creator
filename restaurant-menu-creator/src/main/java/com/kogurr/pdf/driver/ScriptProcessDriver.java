71411994b71ad4c644bd19690bbb1562958702f5
Kogurr <kogurr@gmail.com>
2015-12-07 11:34:37 -0500

    Changes to get the file paths all corrected for installation on an external server.

--- restaurant-menu-creator/src/main/java/com/kogurr/pdf/driver/ScriptProcessDriver.java
+++ restaurant-menu-creator/src/main/java/com/kogurr/pdf/driver/ScriptProcessDriver.java
@@ -10,8 +10,8 @@ public enum ScriptProcessDriver {
 
     INSTANCE;
 
-    private final static String PDF_DIRECTORY = "U:\\Code\\Java\\restaurant-menu-creator\\restaurant-menu-creator\\src\\main\\webapp\\resources\\pdf\\";
-    private final static String SCRIPT_PATH   = "U:\\Code\\Java\\restaurant-menu-creator\\scripts\\TemplateMaker.pl";
+    private final static String PDF_DIRECTORY = "/home/kogurr/java/restaurant-menu-creator/restaurant-menu-creator/src/main/webapp/resources/pdf/";
+    private final static String SCRIPT_PATH   = "/home/kogurr/perl5/menu-maker/TemplateMaker.pl";
 
     public String makeMenu(String templateName, String fileName, Menu menu) {
         try {
